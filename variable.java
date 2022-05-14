525283347fc4661370fa4846b555cc3465049e2d
Harish Kumar <74486673+harishkv11@users.noreply.github.com>
2022-05-14 12:25:29 +0530

    Added variable file

---
+++ variable.java
@@ -0,0 +1,18 @@
+public class variable {
+    public static void main(String args[]){
+         int x; //declaration
+         x=123; //assignment
+         int y = 10; //initialization
+         float z = 3.14f; //Always append f in the end
+         boolean flag = true;
+         char c = 'A';
+         String name = "Harish";
+
+         System.out.println("Value of x : "+x);
+         System.out.println("Value of y : "+y);
+         System.out.println(z);
+         System.out.println(flag);
+         System.out.println(c);
+         System.out.println("Hello "+name);
+    }
+}
