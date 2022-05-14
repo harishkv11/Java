e4ffdfb130c944ad6f6470a5a88cac5d472fe2da
Harish Kumar <74486673+harishkv11@users.noreply.github.com>
2022-05-14 12:16:00 +0530

    Initial Commit

---
+++ first_program.java
@@ -0,0 +1,20 @@
+public class first_program {
+    public static void main(String args[]){
+        //To print with endline. Can be done with \n
+        System.out.println("Hello World");
+
+        //To print without endline
+        System.out.print("Hello World");
+        System.out.print("My name is Harish");
+
+        //This is singleline comment.This will not be printed
+
+        /*
+            This is a multiline comment.
+            This will also not be printed.
+        */
+
+        //To print text with qoutes, use \ in front of it
+        System.out.println("Hey John, it\'s a sunny day.John replied \"Yes, It\'s very bright today\"");
+    }
+}
