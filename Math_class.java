9da8b843c3e6c40da464d42d732806c8da98b9cf
Harish Kumar <74486673+harishkv11@users.noreply.github.com>
2022-05-14 13:04:59 +0530

    Initial commit

---
+++ Math_class.java
@@ -0,0 +1,22 @@
+public class Math_class {
+    public static void main(String args[]){
+        double x=3.14,y=-10;
+        double z;
+
+        z=Math.max(x, y);
+        System.out.println(z);
+
+        y=Math.abs(y);
+        System.out.println(y);
+
+        z=Math.round(x);
+        System.out.println(z);
+
+        z=Math.ceil(x);
+        System.out.println(z);
+
+        y=Math.sqrt(y);
+        System.out.println(y);
+
+    }
+}
