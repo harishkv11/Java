bcc8c9bb7d3cfcc713e97458e18c2b9a8b3d6997
Harish Kumar <74486673+harishkv11@users.noreply.github.com>
2022-05-14 12:46:46 +0530

    Initial commit

---
+++ expressions.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class expressions {
+    public static void main(String args[]){
+        int a=10,b=3;
+        a=a+b; //Addtion
+        a=a-b; //Subtraction
+        a=a*3; //Multiplication
+        a=a/b; //Division
+        a=a%b; //Modular
+        System.out.println("A is "+a+" and B is "+b);
+
+        a++; //increment operator
+        a--; //decrement operator
+
+        System.out.println(a);
+    }
+}
